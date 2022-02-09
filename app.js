7917f4ed40435c7675cc560a99bbf78b246dd60a
kaal-bhairav-cpu <kaalbhairavy@gmail.com>
2022-02-09 22:06:18 +0530

    second commit

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.evn.PORT || 5000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("Server started on port 3000");
 });
